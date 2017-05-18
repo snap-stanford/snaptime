9605e49d51af9900f7459535f4ab1b8466d95eab
Saachi Jain <saachi@stanford.edu>
2017-05-17 17:25:03 -0700

    fixed time compiler issues

--- time_pipeline/SnapTime.hpp
+++ time_pipeline/SnapTime.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <time.h>
+#include <ctime>
 #include <sstream> 
 
 #include "Snap.h"
---
+++ time_pipeline/vw_process.py
@@ -0,0 +1,35 @@
+import os  
+
+def getType(val):
+	if val[0] == 'f' or val[0] == 't':
+		return "BOOLEAN"
+	if val.isdigit():
+		return "INTEGER"
+	if val.replace(".", "").isdigit():
+		return "FLOAT"
+	return "STRING"
+
+def getTypes(directory):
+	types = []
+	for fn in os.listdir(directory):
+		with open(fn, 'rb') as dfile:
+			if "_values.tsv" not in fn:
+				tsvin = csv.reader(dfile, delimiter='\t')
+				for row in tsvin:
+					 type_v = "%s, %s\n" % (fn, getType(row[-1]))
+					 print type_v
+					 types.append(type_v)
+					break
+	return types
+
+def writeOutput(outputDir, data):
+	with open(fn, 'w+') as ofile:
+		for row in data:
+			ofile.write(row)
+
+if __name__ == "__main__":
+	assert len(sys.argv == 3), "wrong number of arguments"
+	input_dir = sys.argv[1]
+	output_dir = sys.argv[2]
+	types = getTypes(input_dir)
+	writeOutput(output_dir, types)
